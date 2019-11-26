c8d1fb02cafba20d37303649bcbb6b0cb6b1bbf6
Sam Brotherton <sbrother@gmail.com>
2019-11-25 18:28:56 -0700

    Added initial migration

--- app/migrations/versions/47dc360e825a_initial_tables.py
+++ app/migrations/versions/47dc360e825a_initial_tables.py
@@ -1,10 +1,3 @@
-"""Initial Tables
-
-Revision ID: 47dc360e825a
-Revises:
-Create Date: 2019-07-28 21:03:19.737020
-
-"""
 from alembic import op
 import sqlalchemy as sa
 
@@ -17,16 +10,83 @@ depends_on = None
 
 
 def upgrade():
-    raise NotImplementedError('Please implement first migration.')
+    op.execute("""
+    CREATE TABLE string_ids (
+      id uuid PRIMARY KEY
+    )
+    """)
 
-    # op.execute("""
-    #     CREATE TABLE foo (bar text);
-    #     """)
+    op.execute("""
+    CREATE TABLE string_content (
+      id uuid REFERENCES string_ids(id) ON DELETE CASCADE,
+      language varchar(5),
+      content text,
+      edited_at timestamp with time zone
+    );
+    """)
+
+    op.execute("""
+    CREATE UNIQUE INDEX ON string_content (id, language);
+    """)
+
+    op.execute("""
+    CREATE TABLE patients (
+      id uuid PRIMARY KEY,
+      given_name uuid REFERENCES string_ids(id) ON DELETE CASCADE,
+      surname uuid REFERENCES string_ids(id) ON DELETE CASCADE,
+      date_of_birth DATE,
+      place_of_birth uuid REFERENCES string_ids(id) ON DELETE CASCADE,
+      edited_at timestamp with time zone
+    );
+    """)
+
+    op.execute("""
+    CREATE TABLE clinics (
+      id uuid PRIMARY KEY,
+      name uuid REFERENCES string_ids(id) ON DELETE CASCADE,
+      edited_at timestamp with time zone
+    );
+    """)
+
+    op.execute("""
+    CREATE TABLE users (
+      id uuid PRIMARY KEY,
+      name uuid REFERENCES string_ids(id) ON DELETE CASCADE,
+      role text,
+      edited_at timestamp with time zone
+    );
+    """)
+
+    op.execute("""
+    CREATE TABLE visits (
+      id uuid PRIMARY KEY,
+      patient_id uuid REFERENCES patients(id) ON DELETE CASCADE,
+      clinic_id uuid REFERENCES clinics(id) ON DELETE CASCADE,
+      provider_id uuid REFERENCES users(id) ON DELETE CASCADE,
+      check_in_timestamp timestamp with time zone,
+      check_out_timestamp timestamp with time zone,
+      edited_at timestamp with time zone
+    );
+    """)
+
+    op.execute("""
+    CREATE TABLE events (
+      id uuid PRIMARY KEY,
+      patient_id uuid REFERENCES patients(id) ON DELETE CASCADE,
+      visit_id uuid REFERENCES visits(id) ON DELETE CASCADE,
+      event_timestamp timestamp with time zone,
+      event_metadata timestamp with time zone
+    );
+    """)
 
 
 def downgrade():
-    raise NotImplementedError('Please implement first migration.')
+    op.execute("DROP TABLE events;")
+    op.execute("DROP TABLE visits;")
+    op.execute("DROP TABLE users;")
+    op.execute("DROP TABLE clinics;")
+    op.execute("DROP TABLE patients;")
+    op.execute("DROP TABLE string_content;")
+    op.execute("DROP TABLE string_ids;")
 
-    # op.execute("""
-    #     DROP TABLE foo;
-    #     """)
+
