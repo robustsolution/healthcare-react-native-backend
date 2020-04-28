68ee1067f92b7a095c9b646b78c64dc6d2fc7297
Sam Brotherton <sbrother@gmail.com>
2020-04-27 21:29:03 -0600

    Factor out DB name and user

--- app/config.py
+++ app/config.py
@@ -18,9 +18,9 @@ if ENV in ('dev_local', 'dev_docker', 'stg'):
 
 if ENV == 'prod':
     FLASK_DEBUG = False
-    PG_USER = 'hikma_prod'
+    PG_USER = os.environ['DB_NAME']
     PG_PASSWORD = os.environ['DB_PASSWORD']
     PG_HOST = 'localhost'
-    PG_DB = 'hikma_prod'
+    PG_DB = os.environ['DB_NAME']
     PHOTOS_STORAGE_BUCKET = os.environ['PHOTOS_STORAGE_BUCKET']
     LOCAL_PHOTO_STORAGE_DIR = '/tmp/hikma_photos'
