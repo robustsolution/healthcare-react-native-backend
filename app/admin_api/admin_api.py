db464921b83db9eaca29f169ce0718cf38676a42
Sam Brotherton <sbrother@gmail.com>
2020-04-29 15:19:55 -0600

    Added create user endpoint

--- app/admin_api/admin_api.py
+++ app/admin_api/admin_api.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, request, jsonify
 from web_util import assert_data_has_keys, admin_authenticated
+from web_errors import WebError
 from users.user import User
-from users.data_access import all_user_data
+from users.data_access import all_user_data, add_user
+from language_strings.language_string import LanguageString
+
+import uuid
+import bcrypt
+import psycopg2.errors
 
 
 admin_api = Blueprint('admin_api', __name__, url_prefix='/admin_api')
@@ -18,5 +24,26 @@ def login():
 @admin_api.route('/all_users', methods=['GET'])
 @admin_authenticated
 def get_all_users(_admin_user):
+    all_users = [User.from_db_row(r).to_dict() for r in all_user_data()]
+    return jsonify({'users': all_users})
+
+
+@admin_api.route('/user', methods=['POST'])
+@admin_authenticated
+def create_user(_admin_user):
+    params = assert_data_has_keys(request, {'email', 'password', 'name', 'role'})
+    if params['role'] not in ['admin', 'provider']:
+        raise WebError('Role must be either "admin" or "provider"', 400)
+
+    id = str(uuid.uuid4())
+    language = params.get('language', 'en')
+    name_str = LanguageString(id=str(uuid.uuid4()), content_by_language={language: params['name']})
+    hashed_password = bcrypt.hashpw(params['password'].encode(), bcrypt.gensalt()).decode()
+    user = User(id, name_str, params['role'], params['email'], hashed_password)
+    try:
+        add_user(user)
+    except psycopg2.errors.UniqueViolation:
+        raise WebError('User already exists', 409)
+
     all_users = [User.from_db_row(r).to_dict() for r in all_user_data()]
     return jsonify({'users': all_users})
