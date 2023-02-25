fa8a9815af202e78f0a6f04230ed289856feede3
crazynavi <ilove.delicious.pizza@gmail.com>
2023-02-25 10:46:53 -0700

    update env variable names

--- app/config.py
+++ app/config.py
@@ -22,9 +22,10 @@ if ENV in ("dev_local", "dev_docker", "stg"):
 
 if ENV == "prod":
     FLASK_DEBUG = False
-    PG_USER = os.environ["DB_NAME"]
+    PG_USER = os.environ["DB_USER"]
+    # PG_USER = os.environ["DB_NAME"]
     PG_PASSWORD = os.environ["DB_PASSWORD"]
-    PG_HOST = os.environ["PG_HOST"]
+    PG_HOST = os.environ["DB_HOST"]
     # PG_HOST = 'localhost'
     PG_DB = os.environ["DB_NAME"]
     PHOTOS_STORAGE_BUCKET = os.environ["PHOTOS_STORAGE_BUCKET"]
